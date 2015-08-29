8af1f9d394d272ade63d4cee64905564d936c013
serandules <serandules@gmail.com>
2015-08-29 16:41:14 +0530

    updating

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var model;
 
 try {
-    model = mongoose.model('Token') || require('/model');
+    model = mongoose.model('Token') || require('./model');
 } catch (e) {
     model = require('./model');
 }
