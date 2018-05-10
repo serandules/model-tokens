56a01e9e92f178e4b5ee9b95831eb5476190330b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-05-10 09:42:08 +0530

    updating

--- model.js
+++ model.js
@@ -20,29 +20,38 @@ var accessibility = 365 * 24 * 60 * 60000;
 var refreshability = 10 * accessibility;
 
 var token = Schema({
-    has: {
-        type: Object,
-        default: {
-            '*': {
-                '': ['*']
-            }
-        }
-    },
-    limits: {
-      type: Object,
-      server: true,
-      value: values.limits()
-    },
-    access: String,
-    accessible: {type: Number, default: accessibility},
-    refresh: String,
-    refreshable: {type: Number, default: refreshability},
-    client: {
-        type: Schema.Types.ObjectId,
-        ref: 'clients',
-        validator: types.ref(),
-        autopopulate: true
+  has: {
+    type: Object,
+    default: {
+      '*': {
+        '': ['*']
+      }
     }
+  },
+  limits: {
+    type: Object,
+    server: true
+  },
+  access: String,
+  accessible: {type: Number, default: accessibility},
+  refresh: String,
+  refreshable: {type: Number, default: refreshability},
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: 'clients',
+    required: true,
+    validator: types.ref(),
+    autopopulate: true
+  },
+  tier: {
+    type: Schema.Types.ObjectId,
+    ref: 'tiers',
+    required: true,
+    validator: types.ref(),
+    value: values.tier(),
+    autopopulate: true,
+    server: true
+  }
 }, {collection: 'tokens'});
 
 token.plugin(mongins);
@@ -53,94 +62,94 @@ token.plugin(mongins.updatedAt);
 token.plugin(autopopulate);
 
 token.methods.accessibility = function () {
-    var exin = this.createdAt.getTime() + this.accessible - new Date().getTime();
-    return exin > 0 ? exin : 0;
+  var exin = this.createdAt.getTime() + this.accessible - new Date().getTime();
+  return exin > 0 ? exin : 0;
 };
 
 token.methods.refreshability = function () {
-    var exin = this.createdAt.getTime() + this.refreshable - new Date().getTime();
-    return exin > 0 ? exin : 0;
+  var exin = this.createdAt.getTime() + this.refreshable - new Date().getTime();
+  return exin > 0 ? exin : 0;
 };
 
 token.methods.can = function (perm, action, o) {
-    var permissions = o.permissions;
-    if (!permissions) {
-        return false;
-    }
-    var user = this.user;
-    var entry = _.find(permissions, function (o) {
-        return String(o.user) === user.id;
-    });
-    if (!entry) {
-        return false;
-    }
-    var actions = entry.actions;
-    if (actions.indexOf(action) === -1) {
-        return false;
-    }
-    var trees = [this.has, this.client.has];
-    return permission.every(trees, perm, action);
+  var permissions = o.permissions;
+  if (!permissions) {
+    return false;
+  }
+  var user = this.user;
+  var entry = _.find(permissions, function (o) {
+    return String(o.user) === user.id;
+  });
+  if (!entry) {
+    return false;
+  }
+  var actions = entry.actions;
+  if (actions.indexOf(action) === -1) {
+    return false;
+  }
+  var trees = [this.has, this.client.has];
+  return permission.every(trees, perm, action);
 };
 
 token.statics.search = function (value, cb) {
-    this.findOne({
-        value: value
-    }).select('createdAt accessible').exec(function (err, token) {
-        cb(err, (err || !token) ? false : token);
-    });
+  this.findOne({
+    value: value
+  }).select('createdAt accessible').exec(function (err, token) {
+    cb(err, (err || !token) ? false : token);
+  });
 };
 
 token.pre('save', function (next) {
-    var that = this;
-    that.allowed = that.allowed || {};
-    permission.permit(that.allowed, 'users:' + that.user, 'read');
-    async.parallel([
-            function (cb) {
-                crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
-                    if (err) {
-                        cb(err);
-                        return;
-                    }
-                    that.access = buf.toString('hex');
-                    cb(null);
-                });
-            },
-            function (cb) {
-                crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
-                    if (err) {
-                        cb(err);
-                        return;
-                    }
-                    that.refresh = buf.toString('hex');
-                    cb(null);
-                });
-            }
-        ],
-        function (err, results) {
-            next(err);
+  var that = this;
+  that.allowed = that.allowed || {};
+  permission.permit(that.allowed, 'users:' + that.user, 'read');
+  async.parallel([
+      function (cb) {
+        crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
+          if (err) {
+            cb(err);
+            return;
+          }
+          that.access = buf.toString('hex');
+          cb(null);
         });
+      },
+      function (cb) {
+        crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
+          if (err) {
+            cb(err);
+            return;
+          }
+          that.refresh = buf.toString('hex');
+          cb(null);
+        });
+      }
+    ],
+    function (err, results) {
+      next(err);
+    });
 });
 
 token.statics.refresh = function (id, done) {
-    var Token = this;
-    Lock.acquire('refresh-' + id, function (err, release) {
-        if (err) {
-            return done(err);
-        }
-        crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
-            if (err) {
-                release();
-                return done(err);
-            }
-            Token.update({_id: id}, {
-                access: buf.toString('hex'),
-                createdAt: Date.now()
-            }, function (err) {
-                release();
-                done(err);
-            });
-        });
+  var Token = this;
+  Lock.acquire('refresh-' + id, function (err, release) {
+    if (err) {
+      return done(err);
+    }
+    crypto.randomBytes(TOKEN_LENGTH, function (err, buf) {
+      if (err) {
+        release();
+        return done(err);
+      }
+      Token.update({_id: id}, {
+        access: buf.toString('hex'),
+        createdAt: Date.now()
+      }, function (err) {
+        release();
+        done(err);
+      });
     });
+  });
 };
 
 /*token.set('toJSON', {
